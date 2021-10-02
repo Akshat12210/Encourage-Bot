f283e1ecc0763d824c464bf8a8e5b8f6c28fe759
Akshat Modani <72137725+Akshat12210@users.noreply.github.com>
2021-10-02 09:48:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import discord
+import os
+import requests#to get data from api (https)
+import json #store data in json file to use easily
+
+
+client=discord.Client()
+
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data=json.loads(response.text)
+  quote=json_data[0]['q']+ " -"+json_data[0]['a']
+  return(quote)
+
+@client.event
+async def on_ready():
+  print('Logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+  # don't respond to ourselves
+  if message.author == client.user:
+    return
+  if message.content.startswith('$inspire'):
+    quote=get_quote()
+    await message.channel.send(quote)
+
+client.run(os.getenv('TOKEN'))
