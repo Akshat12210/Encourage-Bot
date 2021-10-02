805ec75d24c1d692c8bd77b1ed591ac0c6fea150
Akshat Modani <72137725+Akshat12210@users.noreply.github.com>
2021-10-02 09:50:45 +0000

    Simple quote sending bot using

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ client=discord.Client()
 def get_quote():
   response = requests.get("https://zenquotes.io/api/random")
   json_data=json.loads(response.text)
-  quote=json_data[0]['q']+ " -"+json_data[0]['a']
+  quote=json_data[0]['q']+ " -    "+json_data[0]['a']
   return(quote)
 
 @client.event
